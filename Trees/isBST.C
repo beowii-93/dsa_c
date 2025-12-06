cea4f377bc4c7cf229887be29ef1c01e48782309
beowii_93 <lalitha12m143@gmail.com>
2025-12-06 07:15:32 +0530

    Added comments to the BST file

--- Trees/isBST.C
+++ Trees/isBST.C
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <malloc.h>
+//Create a structure for the nodes
 struct node{
     int data;
     struct node* left;
     struct node* right;
 };
-
+//Create a function to create node
 struct node* createnode(int data){
     struct node* n=(struct node*)malloc(sizeof(struct node));
     n->data =data;
@@ -13,19 +14,26 @@ struct node* createnode(int data){
     n->right=NULL;
     return n;
 }
+//previous node pointer for pointing to the previous node
+struct node* prev=NULL;
+
 void inorder(struct node* root){
-    static struct node* prev=NULL;
+    //tO GET THE ascending order of the elements as if it is bst it will give the sorted order
     if(root!=NULL){
         inorder(root->left);
         printf("%d ",root->data);
         inorder(root->right);
     }
 }
+//To check whether the tree is BST or not
 int isBST(struct node* root){
+    //if the root is not null,it will check for the left subtree first
     if(root!=NULL){
         if(!isBST(root->left)){
             return 0;
         }
+        //then later the current node will be checked with the previous node
+        //if the previous node's data is greater than or equal to the current node's data ,then condition fails
         if(prev!=NULL && root->data <= prev->data){
             return 0;
         }
